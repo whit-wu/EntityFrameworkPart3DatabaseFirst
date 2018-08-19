80b48468f4c4ac4c290d536139f174e9d20c9b48
whit-wu <whit.wu@gmail.com>
2018-08-19 12:07:46 -0400

    First Commit

---
+++ EntityFameworkPart3DatabaseFirst/Customer_Config.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace EntityFameworkPart3DatabaseFirst
+{
+    public class CUSTOMER_CONFIG
+    {
+        [StringLength(int.MaxValue, MinimumLength = 2)]
+        public string MIDDLE_NAME { get; set; }
+    }
+
+
+    [MetadataType(typeof(CUSTOMER_CONFIG))]
+    public partial class CUSTOMER
+    {
+    }
+   
+}
---
+++ EntityFameworkPart3DatabaseFirst/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+
+namespace EntityFameworkPart3DatabaseFirst
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            QUARKSEntities ctx = new QUARKSEntities();
+
+            //var quarksQuery = from c in ctx.CUSTOMERS
+            //                  join t in  ctx.TRANSACTIONS
+            //                  on c.CUSTOMER_ID equals t.CUSTOMER_ID
+            //                  where c.MIDDLE_NAME != null
+            //                  select new
+            //                  {
+            //                      c.FIRST_NAME,
+            //                      c.MIDDLE_NAME,
+            //                      c.LAST_NAME,
+            //                      t.TRANSACTION_AMOUNT,
+            //                      t.TRANSACTION_ID
+            //                  };
+
+            //foreach (var result in quarksQuery)
+            //{
+            //    Console.WriteLine("Customer " + result.FIRST_NAME + " " +
+            //        result.MIDDLE_NAME + " " + result.LAST_NAME + " owes "
+            //        + result.TRANSACTION_AMOUNT + " for transaction " + result.TRANSACTION_ID);
+            //}
+
+            Console.Write("Enter ID of customer: ");
+            var CustID = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter first name of customer: ");
+            var FirstName = Console.ReadLine();
+
+            Console.Write("Enter middle name of customer: ");
+            var MiddleName = Console.ReadLine();
+
+            Console.Write("Enter last name of customer: ");
+            var LastName = Console.ReadLine();
+
+            using (ctx)
+            {
+                CUSTOMER cust = new CUSTOMER
+                {
+                    CUSTOMER_ID = CustID,
+                    FIRST_NAME = FirstName,
+                    MIDDLE_NAME = MiddleName,
+                    LAST_NAME = LastName
+                };
+
+                ctx.CUSTOMERS.Add(cust);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
